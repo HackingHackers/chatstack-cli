58ace32f69dd2eb1f735fcf74e6729e80be7767f
undy <52874216+HackingHackers@users.noreply.github.com>
2021-08-23 15:45:56 +0800

    Create class LeanCloudCredentials

---
+++ LeanCloudCredentials.cs
@@ -0,0 +1,10 @@
+namespace ChatStack
+{
+    static class LeanCloudCredentials
+    {
+        // TODO: Add your LeanCloud AppID, AppKey, and REST API server URL (the URL may be null).
+        internal const string AppID = "";
+        internal const string AppKey = "";
+        internal const string Url = "";
+    }
+}
